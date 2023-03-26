177c573907fdf21d98e7c9357a6ec163fa79fec1
Wayne Tan <wtjh00@gmail.com>
2023-03-26 23:34:27 +0800

    fix: update datetime format to confirm to MSSQL

--- DQL/Query_1.sql
+++ DQL/Query_1.sql
@@ -2,5 +2,5 @@ SELECT AVG(PH.PRICE) AS AVG_PRICE
 FROM Price-History AS PH, Books AS B
 WHERE B.PubID = PH.PubID
 AND B.Title = 'Harry Porter Finale'
-AND PH.Start-date = '1 August 2022'
-AND PH.End-date = '31 August 2022'
+AND PH.Start-date = '2022-08-01'
+AND PH.End-date = '2022-08-31'
