8a000226c54b190b122aff613aae631257baddd9
marcusyeoyh <112600925+marcusyeoyh@users.noreply.github.com>
2023-03-27 14:44:40 +0800

    Update Complaints.sql
    Added EID to Complaints.sql

--- DDL/Complaints.sql
+++ DDL/Complaints.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Complaints (
     ComplaintID VARCHAR(255) NOT NULL,
     CustomerID VARCHAR(255) NOT NULL,
+    EmployeeID VARCHAR(255) NOT NULL,
     ComplaintBody VARCHAR(255) NOT NULL, /*Replaces Text*/
     FiledDatetime DATETIME NOT NULL,
     HandledDatetime DATETIME,
