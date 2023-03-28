0c534a8271b3f02cc8d33c9477b3e55641639167
Trigon25 <marccherndiyong2528@gmail.com>
2023-03-29 01:44:35 +0800

    updated query 6

--- DQL/Query_6.sql
+++ DQL/Query_6.sql
@@ -1,11 +1,11 @@
-SELECT TOP 1 X.BookstoreID, SUM(ItemPrice * ItemQty) AS TotalRevenue
+SELECT X.BookstoreID, SUM(ItemPrice * ItemQty) AS TotalRevenue
 FROM ItemsInOrder AS X INNER JOIN OrderStatus AS Y ON 
 	Y.ItemID = X.ItemID
 		AND Y.StockID = X.StockID
 		AND Y.BookstoreID = X.BookstoreID
 		AND Y.PubID = X.PubID
 		AND Y.OrderID = X.OrderID
-WHERE X.OrderDatetime BETWEEN '2022-08-01' AND '2022-08-31'
+WHERE X.OrderDatetime BETWEEN '2022-08-01' AND '2022-09-01'
 	AND Y.OrderState != 'Returned'
 GROUP BY X.BookstoreID
 ORDER BY TotalRevenue DESC
