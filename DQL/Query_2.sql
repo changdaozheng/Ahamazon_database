9b7cd6908f0569be432edea9bf0702f4df7af03f
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2023-03-29 01:18:28 +0800

    modifiy query 2

--- DQL/Query_2.sql
+++ DQL/Query_2.sql
@@ -1,12 +1,16 @@
-SELECT IO.PubID, AVG(IO.Rating) AS Avg_Rating
-FROM ItemsInOrder AS IO, 
-    (SELECT PubID
-        FROM ItemsInOrder 
-        WHERE Rating = 5
-        AND OrderDatetime BETWEEN '1 August 2022' AND '31 August 2022'
-        GROUP BY PubID
-        HAVING COUNT(Rating) >= 10) AS RES
-WHERE IO.PubID = RES.PubID
-GROUP BY IO.PubID
-ORDER BY Avg_Rating
 
+WITH aug_ratings AS (
+    SELECT PubID
+    FROM ItemsInOrder 
+    WHERE Rating = 5
+        AND OrderDatetime BETWEEN '1 August 2022' AND '31 August 2022'
+    GROUP BY PubID
+        HAVING COUNT(Rating) >= 10
+)
+
+SELECT IO.PubID, CAST(AVG(IO.Rating) AS DECIMAL(18,4)) AS Avg_Rating
+FROM ItemsInOrder AS IO
+JOIN aug_ratings AS aug 
+    ON IO.PubID = aug.PubID
+GROUP BY IO.PubID
+ORDER BY Avg_Rating DESC
