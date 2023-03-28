56f382518912129c813f87063b8489a23f5f0848
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2023-03-29 01:51:17 +0800

    modified query 4 to second level

--- DQL/Query_4.sql
+++ DQL/Query_4.sql
@@ -1,5 +1,16 @@
-SELECT TOP 1 C.EmployeeID
-FROM Complaints AS C, Employees AS E
-WHERE C.EmployeeID = E.EmployeeID
-GROUP BY C.EmployeeID
-ORDER BY AVG(DATEDIFF(day, C.FiledDatetime, C.HandledDatetime)) 
+WITH employee_avg AS (
+    SELECT C.EmployeeID, CAST(AVG(DATEDIFF(SECOND, C.FiledDatetime, C.HandledDatetime)) AS DECIMAL(18,4)) AS EmployeeLatency
+    FROM Complaints AS C
+    JOIN Employees AS E
+        ON C.EmployeeID = E.EmployeeID
+    GROUP BY C.EmployeeID
+),
+
+min_latency AS (
+    SELECT EmployeeID, EmployeeLatency, MIN(EmployeeLatency) OVER() AS LowestLatency 
+    FROM employee_avg
+)
+
+SELECT EmployeeID, EmployeeLatency, LowestLatency
+FROM min_latency 
+WHERE EmployeeLatency = LowestLatency
