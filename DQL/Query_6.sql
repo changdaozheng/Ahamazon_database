99429f26fe258b247f74d5ab2bc19a625b50c8cb
Trigon25 <marccherndiyong2528@gmail.com>
2023-03-29 01:46:49 +0800

    reverted query 6

--- DQL/Query_6.sql
+++ DQL/Query_6.sql
@@ -5,7 +5,7 @@ FROM ItemsInOrder AS X INNER JOIN OrderStatus AS Y ON
 		AND Y.BookstoreID = X.BookstoreID
 		AND Y.PubID = X.PubID
 		AND Y.OrderID = X.OrderID
-WHERE X.OrderDatetime BETWEEN '2022-08-01' AND '2022-09-01'
+WHERE X.OrderDatetime BETWEEN '2022-08-01' AND '2022-08-31'
 	AND Y.OrderState != 'Returned'
 GROUP BY X.BookstoreID
 ORDER BY TotalRevenue DESC
