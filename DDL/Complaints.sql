027065ea38669e1fd821c1d2962e828cf5615d80
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2023-03-28 14:06:17 +0800

    modified EID to EmployeeID

--- DDL/Complaints.sql
+++ DDL/Complaints.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Complaints (
     ComplaintID VARCHAR(255) NOT NULL,
     CustomerID VARCHAR(255) NOT NULL,
-    EmployeeID VARCHAR(255) NOT NULL,
+    EmployeeID VARCHAR(255),
     ComplaintBody VARCHAR(255) NOT NULL, /*Replaces Text*/
     FiledDatetime DATETIME NOT NULL,
     HandledDatetime DATETIME,
-    EID VARCHAR(255),
 
     PRIMARY KEY (ComplaintID)
     FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+    FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID)
 )
--- DDL/Employees.sql
+++ DDL/Employees.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Employees (
-    EmployeeID VARCHAR(255),
+    EID VARCHAR(255),
     EmployeeName VARCHAR(255) NOT NULL, /*Replaces Name*/
     Salary DECIMAL(18,2),
     
-    PRIMARY KEY (EmployeeID)
+    PRIMARY KEY (EID)
 )
