7b869e76d7628b476c789bf4cdff11bce8823567
Trigon25 <marccherndiyong2528@gmail.com>
2023-03-29 00:02:37 +0800

    Updated query 3

--- DQL/Query_3.sql
+++ DQL/Query_3.sql
@@ -1,8 +1,7 @@
-SELECT AVG(DATEDIFF(day, X.OrderDatetime, Z.DeliveryDatetime)) AS Avg_Delivery_Time
+SELECT AVG(DATEDIFF(day, X.OrderDatetime, Z.DeliveryDatetime)/1.00) AS Avg_Delivery_Time
 FROM (SELECT O.OrderID, O.OrderDatetime
 	  	FROM Orders AS O
 	 	WHERE O.OrderDatetime BETWEEN '2022-06-01' AND '2022-06-30') AS X,  
 	ItemsInOrder AS Z 
 WHERE Z.DeliveryDatetime IS NOT NULL
 	AND Z.OrderID = X.OrderID
-
