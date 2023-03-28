4dcf7d3eb3d969cfccf9fa0f793e559b0f6491d5
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2023-03-28 23:05:52 +0800

    modified query 8

--- DQL/Query_8.sql
+++ DQL/Query_8.sql
@@ -1,15 +1,14 @@
-SELECT PubID
-FROM Orders os, Items-In-Orders iio
+SELECT TOP 3 PubID
+FROM Orders AS os, ItemsInOrder AS iio
 WHERE os.OrderID = iio.OrderID
-    AND os.Date-time >= ‘2022-08-01’ 
-    AND os.Date-time < ‘2022-09-01’
+    AND os.OrderDatetime >= '2022-08-01'
+    AND os.OrderDatetime < '2022-09-01'
     AND NOT EXISTS
             (SELECT *
-                FROM Items-In-Orders ii, Orders o
+                FROM ItemsInOrder AS ii, Orders o
                 WHERE ii.PubID = iio.PubID
-                    AND o.Date-time = ii.Date-Time
-                AND o.Date-time >= ‘2022-07-01’ 
-                AND o.Date-time < ‘2022-08-01’) 
+                    AND o.OrderDatetime = ii.OrderDatetime
+                    AND o.OrderDatetime >= '2022-07-01'
+                    AND o.OrderDatetime < '2022-08-01') 
 GROUP BY iio.PubID
 ORDER BY SUM(iio.ItemQty) DESC  
-LIMIT 3
