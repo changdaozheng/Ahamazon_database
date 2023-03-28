b79caddc8be070a4d9bc1b6dd3deead43a0997b6
Trigon25 <marccherndiyong2528@gmail.com>
2023-03-28 23:41:04 +0800

    Updated query 3, additional_query_2, and Query_5ii

--- DQL/Query_5ii.sql
+++ DQL/Query_5ii.sql
@@ -1,5 +1,11 @@
-SELECT X.PubID, COUNT(DISTINCT BookstoreID) AS NumBookstores
-FROM StocksInBookstore AS X, Publications AS Y
+With PublicationsInBookstores as (
+    SELECT p.PubID, p.Publisher, sib.BookstoreID ,sib.PubID as sibPubID 
+    FROM Publications AS p
+    LEFT JOIN StocksInBookstore AS sib
+    on sib.PubID = p.PubID 
+)
+
+SELECT PubID, COUNT(DISTINCT BookstoreID) AS NumBookstores
+FROM PublicationsInBookstores
 WHERE Publisher = 'Nanyang Publisher Company'
-    AND X.PubID = Y.PubID
-GROUP BY X.PubID
+GROUP BY PubID
