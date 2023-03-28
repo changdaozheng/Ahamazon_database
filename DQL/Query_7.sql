c2b6b5700c79affcdd246710633dfb6c0ea1ff99
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2023-03-28 22:49:02 +0800

    modified query 7

--- DQL/Query_7.sql
+++ DQL/Query_7.sql
@@ -1,23 +1,19 @@
 WITH max_complaints AS (
-  SELECT CustomerID, COUNT(*) AS num_complaints
+  SELECT TOP 5 CustomerID, COUNT(ComplaintID) AS num_complaints
   FROM Complaints
   GROUP BY CustomerID
   ORDER BY num_complaints DESC
-  LIMIT 1
 ),
-most_expensive_publication AS (
-  SELECT c.CustomerID, MAX(s.Stock_Price) AS max_price
-  FROM Customers c
-  JOIN Orders o ON c.CustomerID = o.CustomerID
-  JOIN Items_In_Orders iio ON o.OrderID = iio.OrderID
-  JOIN Stocks_In_Bookstore s ON iio.StockID = s.StockID
-  WHERE c.CustomerID = (SELECT CustomerID FROM max_complaints)
-  GROUP BY c.CustomerID
+
+purchases AS (
+  SELECT c.CustomerID, num_complaints, s.StockID, s.StockPrice, MAX(s.StockPrice) OVER (PARTITION BY c.CustomerID) AS CustomerMaxPurchase
+  FROM max_complaints AS c
+    JOIN Orders AS o ON c.CustomerID = o.CustomerID
+    JOIN ItemsInOrder AS iio ON o.OrderID = iio.OrderID
+    JOIN StocksInBookstore AS s ON iio.StockID = s.StockID
 )
-SELECT p.Title, p.Year, mep.max_price
-FROM Publication p
-JOIN Stocks_In_Bookstore s ON p.PubID = s.PubID
-JOIN most_expensive_publication mep ON s.BookstoreID = mep.BookstoreID
-WHERE s.Stock_Price = mep.max_price
-ORDER BY mep.max_price DESC
-LIMIT 1;
+
+SELECT CustomerID, num_complaints, StockID, StockPrice
+FROM purchases 
+WHERE StockPrice = CustomerMaxPurchase
+ORDER BY num_complaints DESC
