731fb68485f43a520b67b9083d4745c4ca9b3cb4
marcusyeoyh <112600925+marcusyeoyh@users.noreply.github.com>
2023-03-29 10:52:29 +0800

    Update Query_8.sql
    Redid Query 8

--- DQL/Query_8.sql
+++ DQL/Query_8.sql
@@ -1,16 +1,28 @@
--- 8. Find publications that have never been purchased by any customer in July 2022, but are the top 3 most purchased publications in August 2022.
+WITH aug_sales_ranking AS(
+    SELECT PubID, 
+        SUM(ItemQty) AS Sold, 
+        ROW_NUMBER() OVER(ORDER BY SUM(ItemQty) DESC) AS salesRanking
+    FROM ItemsInOrder
+    WHERE OrderDatetime >= '2022-08-01' 
+        AND OrderDatetime < '2022-09-01'
+    GROUP BY PubID
+),
 
-SELECT TOP 3 PubID
-FROM Orders AS os, ItemsInOrder AS iio
-WHERE os.OrderID = iio.OrderID
-    AND os.OrderDatetime >= '2022-08-01'
-    AND os.OrderDatetime < '2022-09-01'
-    AND NOT EXISTS
-            (SELECT *
-                FROM ItemsInOrder AS ii, Orders o
-                WHERE ii.PubID = iio.PubID
-                    AND o.OrderDatetime = ii.OrderDatetime
-                    AND o.OrderDatetime >= '2022-07-01'
-                    AND o.OrderDatetime < '2022-08-01') 
-GROUP BY iio.PubID
-ORDER BY SUM(iio.ItemQty) DESC  
+jul_not_bought AS (
+    SELECT PubID
+    FROM Publications
+
+    EXCEPT 
+
+    SELECT PubID
+    FROM ItemsInOrder
+    WHERE OrderDatetime >= '2022-07-01' 
+        AND OrderDatetime < '2022-08-01'
+    GROUP BY PubID
+)
+
+SELECT sr.PubID
+FROM aug_sales_ranking AS sr, jul_not_bought AS nb
+WHERE sr.PubID = nb.PubID
+    AND sr.salesRanking <4
+ORDER BY Sold DESC
